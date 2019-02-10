7eb909413e33c028030092495f15c30fa7172ec5
Jose L Saboya Rosales <joseluissaboya@Joses-MBP.home>
2019-02-10 04:40:18 -0500

    The logic was finished, GIFs Folder created and all the demo gifs were uploaded

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # liri-node-app
 
 * LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, 
-  LIRI is a _Language_Interpretation and Recognition Interface. 
+
 * LIRI will be a command line node app that takes in parameters and gives you back data.
+* There are four commands:
+  - spotify-this-song:
+    -takes what ever you typed after the command and searches the spotify DB
+    -If no search query was added, there is a default value for back up
+  - movie-this
+    - requires the user to input a name of a movie, which will 
+  - check-the-market
+  - do-what-it-says
+
+
+![](./GIFS/check-themarket.gif)
--- liri.js
+++ liri.js
@@ -6,178 +6,175 @@ var axios = require("axios");
 var fs = require("fs");
 
 // setting the keys required
+var spotify = new Spotify(keys.spotify);
 var vantage = keys.vantage;
 const alpha = require("alphavantage")({
   key: vantage
 });
 
-// Commands @ process.argv[2]
-var spotify_Command = "spotify-this-song";
-var OMDB_Command = "movie-this";
-var check_Stock_Market = "check-the-market";
-var do_It = "do-what-it-says";
+// var search_Song = process.argv.splice(3).join(" ");
 
-var spotify = new Spotify(keys.spotify);
 
-command_Valaidation();
 
-function command_Valaidation() {
-  if (process.argv[2] === spotify_Command) {
-    var search_Song = process.argv.splice(3).join(" ");
-    spotify_Search(search_Song);
-  }
-
-  if (process.argv[2] === OMDB_Command) {
-    var search_Movie = process.argv.splice(3).join("+");
-    OMDB_Search(search_Movie);
-  }
-  if (process.argv[2] === check_Stock_Market) {
-    // Obtains and saves the symbol inputted
-    var my_Symbol = (process.argv[3]).toUpperCase();
-    stock_Market_Search(my_Symbol);
-  }
-
-  if (process.argv[2] === do_It) {
-    follow_Random_Text();
-  }
-}
-
-function spotify_Search(search_Song) {
-  // If there is no input for a song, it calls it's default
-  if (search_Song === "") {
-    spotify
-      .search({
-        type: "track",
-        query: "The Sign Ace of Base"
-      })
-      .then(function (response) {
-        // Local variables
-        var artist_Name = response.tracks.items[0].artists[0].name;
-        var song_Title = response.tracks.items[0].name;
-        var preview_Url = response.tracks.items[0].preview_url;
-        var album_Name = response.tracks.items[0].album.name;
-        // Displayong the API call results
-        console.log(
-          "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
-          "\nArtist: " + artist_Name +
-          "\nSong Title: " + song_Title +
-          "\nPreview URL: " + preview_Url +
-          "\nAlbum: " + album_Name +
-          "\n\n-----------------------------------------------------------------------------\n");
-      })
-      .catch(function (err) {
+// Command Object
+var command = {
+  // searches for a particular track and displays the results
+  spotify_this_song: function (search) {
+    // var search_Song = process.argv.splice(3).join(" ");
+    // If there is no input for a song, it calls it's default
+    if (search === "") {
+      spotify
+        .search({
+          type: "track",
+          query: "The Sign Ace of Base"
+        })
+        .then(function (response) {
+          // Collects important info from the track and calls the print function 
+          command.spotify_Resp(response);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    } else {
+      spotify
+        .search({
+          type: "track",
+          query: search
+        })
+        .then(function (response) {
+          // Collects important info from the track and calls the print function
+          command.spotify_Resp(response);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
+  },
+  // searches for a particular movie and displays the results
+  movie_this: function () {
+    // API KEY
+    var omdb = keys.omdb;
+    // Search Term
+    var search_Movie = process.argv.splice(3).join(" ");
+    console.log("test "+search_Movie);
+    
+    // Components of query URL
+    var mov_Url = {
+      first_Part: "http://www.omdbapi.com/?t=",
+      search: search_Movie,
+      add_Ons: "&y=&plot=short&apikey=",
+      API_Key: omdb.key
+    };
+    // This Triggers the Default value for a movie
+    if (search_Movie === "") {
+      axios
+        .get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=" + mov_Url.API_Key)
+        .then(function (response) {
+          command.OMDB_Resp(response);
+        });
+    } // Makes a request based on the User's Input
+    else {
+      axios
+        .get(mov_Url.first_Part + mov_Url.search + mov_Url.add_Ons + mov_Url.API_Key)
+        .then(function (response) {
+          command.OMDB_Resp(response);
+        });
+    }
+  },
+  // checks the the performace of the symbol inserted for today
+  check_the_market: function () {
+    var my_Symbol = process.argv[3].toUpperCase();
+    // Generates the request to the Alpha Vantage API
+    alpha.data.daily(my_Symbol).then(function (response) {
+      command.alpha_Vantage_Resp(response);
+    });
+  },
+  // 
+  do_what_it_says: function () {
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+      if (err) {
         console.log(err);
-      });
-  } else {
-    spotify
-      .search({
-        type: "track",
-        query: search_Song
-      })
-      .then(function (response) {
-        // local variables
-        var artist_Name = response.tracks.items[0].artists[0].name;
-        var song_Title = response.tracks.items[0].name;
-        var preview_Url = response.tracks.items[0].preview_url;
-        var album_Name = response.tracks.items[0].album.name;
+      }
+      var text_Array = data.split(",");
+      var text_Command = text_Array[0];
+      var search = text_Array[1].slice(1, -1);// removes the quotes around the word
+     
+      if (text_Command === "spotify-this-song") {
+        command.spotify_this_song(search);
+      }
+    });
+  },
 
-        console.log(
-          "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
-          "\nArtist: " + artist_Name +
-          "\nSong Title: " + song_Title +
-          "\nPreview URL: " + preview_Url +
-          "\nAlbum: " + album_Name +
-          "\n\n-----------------------------------------------------------------------------\n");
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
-}
-
-function OMDB_Search(search_Movie) {
-  // API KEY
-  var omdb = keys.omdb;
-  // Components of query URL
-  var mov_Url = {
-    first_Part: "http://www.omdbapi.com/?t=",
-    search: search_Movie,
-    add_Ons: "&y=&plot=short&apikey=",
-    API_Key: omdb.key
-  };
-  // This Triggers the Default value for a movie
-  if (search_Movie === "") {
-    axios
-      .get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=" + mov_Url.API_Key)
-      .then(function (response) {
-        // local variables
-        var resp = response.data;
-        var title = resp.Title;
-        var year = resp.Year;
-        var IMDB_Rating = resp.Ratings[0].Value.split("/").splice(0, 1) + " out of 10";
-        var RT_Rating = resp.Ratings[1].Value.split("/").splice(0, 1);
-        var country = resp.Country;
-        var language = resp.Language;
-        var plot = resp.Plot;
-        var actors = resp.Actors;
-        // Display the result of the request
-        console.log(
-          "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
-          "\nMovie Title: " + '"' + title + '"' +
-          "\nYear Released: " + year +
-          "\nIMDB Rating: " + IMDB_Rating +
-          "\nRotten Tomatoes Rating: " + RT_Rating +
-          "\nMade In: " + country +
-          "\nLanguage: " + language +
-          "\nPlot: " + '"' + plot + '"' +
-          "\nActors: " + actors +
-          "\n\n-----------------------------------------------------------------------------\n");
-      });
-  } // Makes a request based on the User's Input
-  else {
-    axios
-      .get(mov_Url.first_Part + mov_Url.search + mov_Url.add_Ons + mov_Url.API_Key)
-      .then(function (response) {
-        // local variable
-        var resp = response.data;
-        var title = resp.Title;
-        var year = resp.Year;
-        var IMDB_Rating = resp.Ratings[0].Value.split("/").splice(0, 1) + " out of 10";
-        var RT_Rating = resp.Ratings[1].Value.split("/").splice(0, 1);
-        var country = resp.Country;
-        var language = resp.Language;
-        var plot = resp.Plot;
-        var actors = resp.Actors;
-        // Display the Result of the request
-        console.log(
-          "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
-          "\nMovie Title: " + '"' + title + '"' +
-          "\nYear Released: " + year +
-          "\nIMDB Rating: " + IMDB_Rating +
-          "\nRotten Tomatoes Rating: " + RT_Rating +
-          "\nMade In: " + country +
-          "\nLanguage: " + language +
-          "\nPlot: " + '"' + plot + '"' +
-          "\nActors: " + actors +
-          "\n\n-----------------------------------------------------------------------------\n");
-      });
-  }
-}
-
-function stock_Market_Search(my_Symbol) {
-
-  // Generates the request to the Alpha Vantage API
-  alpha.data.daily(my_Symbol).then(function (data) {
+  // DATA COLLECTION/MANIPULATION 
+  // ========================================================================================
+  // Collects important info from the track and calls the print function
+  spotify_Resp: function (response) {
+    // local variables
+    // console.log(response);
+    
+    var artist_Name = response.tracks.items[0].artists[0].name;
+    // console.log(artist_Name);
+    
+    var song_Title = response.tracks.items[0].name;
+    var preview_Url = response.tracks.items[0].preview_url;
+    var album_Name = response.tracks.items[0].album.name;
+    command.spotify_Disp(artist_Name, song_Title, preview_Url, album_Name);
+  },
+  OMDB_Resp: function (response) {
+    // local variable
+    var resp = response.data;
+    var title = resp.Title;
+    var year = resp.Year;
+    var IMDB_Rating = resp.Ratings[0].Value.split("/").splice(0, 1) + " out of 10";
+    var RT_Rating = resp.Ratings[1].Value.split("/").splice(0, 1);
+    var country = resp.Country;
+    var language = resp.Language;
+    var plot = resp.Plot;
+    var actors = resp.Actors;
+    command.OMDB_Disp(title, year, IMDB_Rating, RT_Rating, country, language, plot, actors);
+  },
+  alpha_Vantage_Resp: function (response) {
     // Local Variables
-    var title = data["Meta Data"]["1. Information"];
-    var symbol = data["Meta Data"]["2. Symbol"];
-    var last_Refresh = data["Meta Data"]["3. Last Refreshed"];
-    var time_Zone = data["Meta Data"]["5. Time Zone"];
-    var date = data["Time Series (Daily)"][last_Refresh];
-    var open = data["Time Series (Daily)"][last_Refresh]["1. open"];
-    var high = data["Time Series (Daily)"][last_Refresh]["2. high"];
-    var low = data["Time Series (Daily)"][last_Refresh]["3. low"];
-    var close = data["Time Series (Daily)"][last_Refresh]["4. close"];
+    var title = response["Meta Data"]["1. Information"];
+    var symbol = response["Meta Data"]["2. Symbol"];
+    var last_Refresh = response["Meta Data"]["3. Last Refreshed"];
+    var time_Zone = response["Meta Data"]["5. Time Zone"];
+    var date = [last_Refresh.split(" ")[0]];
+    var open = response["Time Series (Daily)"][date]["1. open"];
+    var high = response["Time Series (Daily)"][date]["2. high"];
+    var low = response["Time Series (Daily)"][date]["3. low"];
+    var close = response["Time Series (Daily)"][date]["4. close"];
+    command.alpha_Vantage_Disp(title, last_Refresh, time_Zone, symbol, open, high, low, close);
+  },
+
+  // DATA RESPONSE DISPLAYED
+  // ========================================================================================
+  // Prints the Spotify Results
+  spotify_Disp: function (artist_Name, song_Title, preview_Url, album_Name) {
+    // Displayong the API call results
+    console.log(
+      "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
+      "\nArtist: " + artist_Name +
+      "\nSong Title: " + song_Title +
+      "\nPreview URL: " + preview_Url +
+      "\nAlbum: " + album_Name +
+      "\n\n-----------------------------------------------------------------------------\n");
+  },
+  OMDB_Disp: function (title, year, IMDB_Rating, RT_Rating, country, language, plot, actors) {
+    // Display the Result of the request
+    console.log(
+      "\n-------------------------------- SEARCH RESULT --------------------------------\n" +
+      "\nMovie Title: " + '"' + title + '"' +
+      "\nYear Released: " + year +
+      "\nIMDB Rating: " + IMDB_Rating +
+      "\nRotten Tomatoes Rating: " + RT_Rating +
+      "\nMade In: " + country +
+      "\nLanguage: " + language +
+      "\nPlot: " + '"' + plot + '"' +
+      "\nActors: " + actors +
+      "\n\n-----------------------------------------------------------------------------\n");
+  },
+  alpha_Vantage_Disp: function (title, last_Refresh, time_Zone, symbol, open, high, low, close) {
     // Display the Results
     console.log(
       "\n--------------------------- Today's Market Summary ---------------------------\n" +
@@ -190,20 +187,23 @@ function stock_Market_Search(my_Symbol) {
       "\nLow      : " + "$ " + Number.parseFloat(low).toFixed(2) +
       "\nClose    : " + "$ " + Number.parseFloat(close).toFixed(2) +
       "\n\n-----------------------------------------------------------------------------\n");
-  });
+  }
 }
 
-function follow_Random_Text() {
-  fs.readFile("./random.txt", "utf8", function (err, data) {
-    if (err) {
-      console.log(err);
-    }
-    var text_Array = data.split(",");
-    var command = text_Array[0];
-    search_Song = text_Array[1];
-
-    if (command === spotify_Command) {
-      spotify_Search(search_Song);
-    }
-  })
+// Evaluates the command inputed and triggers the proper function inside of the command Object
+if (typeof command[process.argv[2]] === "function") {
+  // if the command does not equal spotify_this_song and does not equal do_what_it_says run the mathing method within the command object
+  if (process.argv[2] !== "spotify_this_song" && process.argv[2] !== "do_what_it_says") {
+    command[process.argv[2]]();
+  }
+  // if the command equal spoti_this_song it collects the search term from terminal input and calls the method spotify_this_song with and passes the search term as a parametere
+  if (process.argv[2] === "spotify_this_song") {
+    let search = process.argv.splice(3);
+    search = search.join(" ");
+    command[process.argv[2]](search);
+  } 
+  // if the command is do_what_it_says that method is called
+  else if (process.argv[2] === "do_what_it_says") {
+    command[process.argv[2]]();
+  }
 }
